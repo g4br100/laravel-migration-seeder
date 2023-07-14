2108d6ec3c031530742ef608afbaf96d36fc9c44
user1 <user@email.com>
2023-07-14 13:05:56 +0200

    Inizio

---
+++ database/migrations/2023_07_13_122517_create_trains_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+
+            $table->string("agency", 50);
+            $table->string("departure_station", 50);
+            $table->string("arrival_station", 50);
+            $table->Time('departure_time');
+            $table->Time('arrival_time');
+            $table->char("train_code", 8);
+            $table->tinyInteger("number_of_carriages");
+            $table->boolean("in_time");
+            $table->boolean("deleted");
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+};
